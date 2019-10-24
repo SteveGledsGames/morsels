1bb9a379abe6fdefaa46062366b77b80966f8048
stevegleds <stevegleds@gmail.com>
2019-10-24 22:38:41 +0100

    it works but doesn't pass because no ""

--- fix_csv/fix_csv.py
+++ fix_csv/fix_csv.py
@@ -2,6 +2,14 @@ import csv
 import sys
 
 with open(sys.argv[1], 'rt') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print(row)
+    with open(sys.argv[2], 'wt') as o:
+        reader = csv.reader(f)
+        writer = csv.writer(o)
+        for row in reader:
+            changed_row = [s.replace('|', ',') for s in row]
+            # print(row)
+            # # changed_row = str(row).replace('|', ',')
+            # print(changed_row)
+            writer.writerow(changed_row)
+
+            
